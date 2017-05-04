d9f91a6c2d0b3834f09d8d560e8792488cac8421
javierplanta <javierkns@gmail.com>
2017-05-04 20:09:22 -0300

    Primer commit Back-end <1.4.1>

---
+++ src/main/java/backEnd.java
@@ -0,0 +1,10 @@
+public class backEnd {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Soy Backend\n");
+        
+    }
+}
