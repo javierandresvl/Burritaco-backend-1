059153772e17909993e3b1907298c6c82aceeed3
javierplanta <javierkns@gmail.com>
2017-05-09 17:21:55 -0300

    Se ha implementado lucene

--- src/main/java/backEnd.java
+++ src/main/java/backEnd.java
@@ -1,10 +1,20 @@
+
+import java.io.IOException;
+import org.apache.lucene.queryparser.classic.ParseException;
+
 public class backEnd {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException {
         
         System.out.println("Soy Backend\n");
+        
+        Searcher s = new Searcher();
+        s.createIndex();
+        
+        //busqueda de prueba
+        s.search("South");
         
     }
 }
